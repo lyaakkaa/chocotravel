953efb5e0d9f2aad08b33d941cd4d99bd1a57e6f
lyaakkaa <leila.bekturgan@gmail.com>
2023-11-29 11:52:17 +0600

    cancel tick

--- profile.php
+++ profile.php
@@ -211,11 +211,10 @@ session_start();
                                             echo '    <span class="airline airlineslip">' . $ticket_data['airline_name'] . '</span>';
                                             echo '    <div class="content">';
                                             echo '      <span class="jfk">' . $ticket_data['departure_city_name'] . '</span>';
-                                            echo '      <span class="plane"><?xml version="1.0" ?><svg clip-rule="evenodd" fill-rule="evenodd" height="60" width="60" image-rendering="optimizeQuality" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg"><g stroke="#222"><line fill="none" stroke-linecap="round" stroke-width="30" x1="300" x2="55" y1="390" y2="390"/><path d="M98 325c-9 10 10 16 25 6l311-156c24-17 35-25 42-50 2-15-46-11-78-7-15 1-34 10-42 16l-56 35 1-1-169-31c-14-3-24-5-37-1-10 5-18 10-27 18l122 72c4 3 5 7 1 9l-44 27-75-15c-10-2-18-4-28 0-8 4-14 9-20 15l74 63z" fill="#222" stroke-linejoin="round" stroke-width="10"/></g></svg></span>';
-                                            echo '      <span class="sfo">SFO</span>';
-                                            echo '      <span class="jfk jfkslip">JFK</span>';
-                                            echo '      <span class="plane planeslip"><?xml version="1.0" ?><svg clip-rule="evenodd" fill-rule="evenodd" height="50" width="50" image-rendering="optimizeQuality" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg"><g stroke="#222"><line fill="none" stroke-linecap="round" stroke-width="30" x1="300" x2="55" y1="390" y2="390"/><path d="M98 325c-9 10 10 16 25 6l311-156c24-17 35-25 42-50 2-15-46-11-78-7-15 1-34 10-42 16l-56 35 1-1-169-31c-14-3-24-5-37-1-10 5-18 10-27 18l122 72c4 3 5 7 1 9l-44 27-75-15c-10-2-18-4-28 0-8 4-14 9-20 15l74 63z" fill="#222" stroke-linejoin="round" stroke-width="10"/></g></svg></span>';
-                                            echo '      <span class="sfo sfoslip">SFO</span>';
+                                            echo '      <span class="sfo">' . $ticket_data['arrival_city_name'] . '</span>';
+                                            echo '      <span class="jfk jfkslip">' . $ticket_data['departure_city_name'] . '</span>';
+
+                                            echo '      <span class="sfo sfoslip">' . $ticket_data['arrival_city_name'] . '</span>';
                                             echo '      <div class="sub-content">';
                                             echo '        <span class="name">PASSENGER NAME<br><span>' . $ticket_data['passenger_first_name'] . ' ' . $ticket_data['passenger_last_name'] .'</span></span>';
                                             echo '        <span class="flight">FLIGHT N&deg;<br><span>' . $ticket_data['flight_number'] .'</span></span>';
